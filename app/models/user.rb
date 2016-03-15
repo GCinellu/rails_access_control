be07c33bb89c949e2e237f8ee878b149cbcdd3d9
GCinellu <gcinellu@gmail.com>
2016-03-16 00:20:28 +0100

    Roles control

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,24 @@ class User < ActiveRecord::Base
 
   validate :has_valid_role?, on: :save
 
-  def self.valid_roles?
+  def self.valid_roles
     ['common', 'administrator', 'manager', 'developer', 'marketeer', 'customer_care', 'owner']
-  end
-
-  def can_access_resource?(company)
-    self.is_administrator? or (self.roles.include?('owner') and self.company == company)
   end
 
   def is_administrator?
     self.roles.include?('administrator')
+  end
+
+  def is_owner?(company)
+    self.is_administrator? or (self.roles.include?('owner') and self.company == company)
+  end
+
+  def is_owner_or_admin?(company)
+    self.is_owner?(company) or self.is_administrator?
+  end
+
+  def is_within_company?(company)
+    self.company == company
   end
 
   private
